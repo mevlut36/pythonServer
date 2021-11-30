aa2b99dc7615add867ca43e1649c8acd6a77747a
Mevlut <36679013+mevlut36@users.noreply.github.com>
2021-11-30 10:29:49 +0100

    Create Form.py

---
+++ Form.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+from http.server import BaseHTTPRequestHandler, HTTPServer
+import HTMLClass
+
+hostName = "localhost"
+serverPort = 8081
+
+
+class MyServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        html = HTMLClass.MyServer
+        self.send_response(200)
+        self.end_headers()
+        html.start_html(self)
+        html.start_head(self)
+        self.wfile.write(bytes("<title>Form</title>", "utf-8"))
+        self.wfile.write(bytes("<meta charset='utf-8'>", "utf-8"))
+        html.link(self, "data:;base64,iVBORw0KGgo=", "icon")
+        html.bootstrap(self)
+        html.end_head(self)
+        # <body>
+        self.wfile.write(bytes("<body>", "utf-8"))
+        html.div(self, "d-flex", "wrapper")
+        html.div(self, "container-fluid", "")
+        html.div(self, "col-md-3", "")
+        html.form(self, "POST", "", "form-control", [html.label(self, "name", "Votre nom:"), html.input(self, "text", "name", "form-control", "name", "Votre nom"),
+                                                     html.label(self, "mail", "Votre E-Mail:"), html.input(self, "text", "mail", "form-control", "mail", "Votre E-mail"),
+                                                     html.label(self, "password", "Votre Mot de passe:"), html.input(self, "password", "password", "form-control", "password", "Votre Mot de passe")])
+        html.end_form(self)
+        html.end_div(self)
+        html.end_div(self)
+        html.end_div(self)
+        self.wfile.write(bytes("</body></html>", "utf-8"))
+
+        # </body>
+
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostName, serverPort), MyServer)
+    print("Server start http://%s:%s" % (hostName, serverPort))
+
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stop")
