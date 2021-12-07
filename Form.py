74301632a947e7809263f31fc13d94d509576086
Mevlut <36679013+mevlut36@users.noreply.github.com>
2021-12-07 11:15:06 +0100

    Update for request

--- Form.py
+++ Form.py
@@ -7,8 +7,10 @@ serverPort = 8081
 
 
 class MyServer(BaseHTTPRequestHandler):
-    def do_GET(self):
+    def _set_response(self, data='', method='GET'):
         html = HTMLClass.MyServer
+        if method == 'POST':
+            data = data.decode()
         self.send_response(200)
         self.end_headers()
         html.start_html(self)
@@ -25,18 +27,31 @@ class MyServer(BaseHTTPRequestHandler):
         html.div(self, "col-md-3", "")
         html.h4(self, "Connexion", "", True)
         html.form(self, "POST", "", "form-control",
-                  [html.label(self, "name", "Votre nom:"), html.input(self, "text", "name", "form-control", "name", "Votre nom"),
-                   html.label(self, "mail", "Votre E-Mail:"), html.input(self, "text", "mail", "form-control", "mail", "Votre E-mail"),
-                   html.label(self, "password", "Votre Mot de passe:"), html.input(self, "password", "password", "form-control", "password", "Votre Mot de passe"),
-                   html.div(self, "form-check", ""), html.checkbox(self, "checkbox", "remember"), html.label(self, "remember", "Se rappeler de moi"), html.end_div(self),
-                   html.a(self, "Update", "form-control btn btn-primary", "index.py", "button")])
+                  [html.label(self, "name", "Votre nom:", True),
+                   html.input(self, "text", "name", "form-control", "name", "Votre nom", True),
+                   html.label(self, "mail", "Votre E-Mail:", True),
+                   html.input(self, "email", "mail", "form-control", "mail", "Votre E-mail", True),
+                   html.label(self, "password", "Votre Mot de passe:", True),
+                   html.input(self, "password", "password", "form-control", "password", "Votre Mot de passe", True),
+                   html.div(self, "form-check", "", True), html.checkbox(self, "checkbox", "remember", True),
+                   html.label(self, "remember", "Se rappeler de moi", True), html.end_div(self, True),
+                   html.decode_data(self, data, 'password', True),
+                   html.button(self, "submit", "form-control btn btn-primary", "Update", True)])
         html.end_form(self)
         html.end_div(self)
         html.end_div(self)
         html.end_div(self)
         self.wfile.write(bytes("</body></html>", "utf-8"))
 
-        # </body>
+    def do_GET(self):
+        self._set_response('')
+
+    # </body>
+
+    def do_POST(self):
+        content_length = int(self.headers['Content-Length'])
+        post_data = self.rfile.read(content_length)
+        self._set_response(post_data, 'POST')
 
 
 if __name__ == "__main__":
